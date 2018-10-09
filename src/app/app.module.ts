e2eb1935327e4945b519e2b079e72da460defe59
LakshithaMadushan <LakshithaMadushan@users.noreply.github.com>
2018-10-09 10:36:38 +0530

    Init-Add-Spinner-Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { ArticleContainerComponent } from './article-container/article-container.component';
@@ -13,6 +13,7 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddNewArticleComponent } from './add-new-article/add-new-article.component';
 import { AdminMenuComponent } from './admin-menu/admin-menu.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
@@ -26,11 +27,13 @@ import { AdminMenuComponent } from './admin-menu/admin-menu.component';
     FooterComponent,
     AboutMeComponent,
     AddNewArticleComponent,
-    AdminMenuComponent
+    AdminMenuComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
