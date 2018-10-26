7474d4df471a522ed0d0c7ab4bf3f60e9643e8fc
LakshithaMadushan <LakshithaMadushan@users.noreply.github.com>
2018-10-26 20:17:20 +0530

    Init-Auth-Service

---
+++ src/app/auth-service/auth.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {AngularFireDatabase} from 'angularfire2/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  authState: any = null;
+
+  constructor(private afAuth: AngularFireAuth, private db: AngularFireDatabase) {
+    this.afAuth.authState.subscribe((auth) => {
+      this.authState = auth
+    });
+  }
+
+  // Returns true if user is logged in
+  get authenticated(): boolean {
+    return this.authState !== null;
+  }
+
+  // Returns current user data
+  get currentUser(): any {
+    return this.authenticated ? this.authState : null;
+  }
+}
